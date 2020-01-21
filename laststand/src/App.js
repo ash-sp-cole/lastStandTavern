c152e1f28125a3483517216f1e2644e5df17d597
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-01-21 10:15:41 +0100

    review

---
+++ laststand/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import person from './Person/Person';
+
+
+
+const app = () => {
+
+    return (
+<div>
+        <div> App  component</div>
+
+            <person/>
+
+            </div>
+    );
+};
+
+export default App;
