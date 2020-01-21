3f4019e8360646c1ea49219086fc5b0a2532e20a
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-01-21 09:58:38 +0100

    es6 functions

---
+++ laststand/src/Person/Person.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const person = () => {
+
+
+return (
+
+    <div> Hello w0rld</div>
+
+
+);
+};
+
+export default person;
--- laststand/src/index.js
+++ laststand/src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 
+const App = () => {
+return (
+
+    <div className ="ui-container comments">
+
+<div>
+     <a href ="/" className ="avatar">
+    <img alt="avatar" />
+    </a>
+</div>
+
+
+
+    </div>
+
+
+
+)
+
+};
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
