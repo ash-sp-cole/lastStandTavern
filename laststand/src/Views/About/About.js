d810eabf9cf0b13ae05a702c4bb7417091925e16
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-01-31 21:46:51 +0100

    Link to match

---
+++ laststand/src/Views/About/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const About = () => {
+
+return (
+<div>
+
+    <h3> ABOUT view</h3>
+
+</div>
+
+)
+
+}
+
+export default About;
---
+++ laststand/src/Views/TopicList/TopicList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+const TopicList = () => {
+ console.log(match.url);
+return (
+<div>
+<h3>
+    Topic List View
+</h3>
+<ul>
+<li>
+    <Link to={'${match.url}/Topic1'}>Topic 1</Link>
+</li>
+<li><Link to={'${match.url}/Topic 2'}>Topic 2</Link>
+</li>
+<li>
+    <Link to={'${match.url}/Topic 3'}>Topic 3</Link>
+</li>
+
+</ul>
+
+
+</div>
+
+
+)
+
+
+
+
+
+}
+
+export default TopicList;
