09da1ded33fa143d762df77181f347e5aedfd43b
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-02-02 13:49:15 +0100

    ant ui

--- laststand/src/Components/Header/index.js
+++ laststand/src/Components/Header/index.js
@@ -4,19 +4,21 @@ import {Link} from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import './navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome} from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
 
-
+  const element = <FontAwesomeIcon icon={faHome} />
 
    return ( 
 <div className="style"> 
 
-
+<h3 className="right">Last Stand Tavern</h3>
 
 
 <Navbar.Collapse class="navbar bg-transparent navbar-fixed-top " >
-  <Navbar.Brand as={Link} to="/" className="style" >Home</Navbar.Brand>
+  <Navbar.Brand as={Link} to="/" className="style" >{element}</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
     <Nav className="mr-auto"className="justify-content-end"  >
@@ -34,3 +36,4 @@ const NavBar = () => {
 )}
 
 export default NavBar;
+
--- laststand/src/Views/About/index.js
+++ laststand/src/Views/About/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
 import './about.css';
+import { Carousel } from 'antd';
 const About = (props) => {
 
 
@@ -17,8 +18,28 @@ DESTINATION!!</h1>
   </p>
   <p>
     <Button variant="primary">Learn more about our history</Button>
+
   </p>
 </Jumbotron>
+
+
+ReactDOM.render(
+  <Carousel effect="fade">
+    <div>
+      <h3>1</h3>
+    </div>
+    <div>
+      <h3>2</h3>
+    </div>
+    <div>
+      <h3>3</h3>
+    </div>
+    <div>
+      <h3>4</h3>
+    </div>
+  </Carousel>,
+  mountNode,
+);
 </div>
 
 )
