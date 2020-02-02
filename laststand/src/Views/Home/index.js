0a51894138b0cc5339267f94ad459514c13dd87a
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-02-02 16:57:54 +0100

    react-native

--- laststand/src/Views/Home/index.js
+++ laststand/src/Views/Home/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import './home.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import FadeInView from '../Animate/index';
+
 
 const Home = (props) => {
-console.log(props);
+
+
+
+
+
     return (
 
 <div> 
     <container className="intro">
     <Row className="rowIntro">
-         
-    <Col></Col>   
+    
+    <Col><FadeInView/></Col>   
     <Col>Eat</Col>
     <Col>Đrink</Col>
     <Col>Vίsίt</Col>
