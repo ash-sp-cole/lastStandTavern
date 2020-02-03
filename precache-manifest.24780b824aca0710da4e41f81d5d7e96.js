d6581aeb2f7ed0822f666a435bd24c26fba6c904
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-02-03 16:12:04 +0100

    Updates

---
+++ precache-manifest.24780b824aca0710da4e41f81d5d7e96.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67eeb5e67be4ff6652ac163f5bdbff0e",
+    "url": "/ash-sp-cole/lastStandTavern/index.html"
+  },
+  {
+    "revision": "deabfa4a443c4ae0cca6",
+    "url": "/ash-sp-cole/lastStandTavern/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "313db4b26ebfd9dcc3ee",
+    "url": "/ash-sp-cole/lastStandTavern/static/css/main.4b6ee5ed.chunk.css"
+  },
+  {
+    "revision": "deabfa4a443c4ae0cca6",
+    "url": "/ash-sp-cole/lastStandTavern/static/js/2.31bdb3f8.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/ash-sp-cole/lastStandTavern/static/js/2.31bdb3f8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "313db4b26ebfd9dcc3ee",
+    "url": "/ash-sp-cole/lastStandTavern/static/js/main.25e3f4a2.chunk.js"
+  },
+  {
+    "revision": "1062d91df53e6b79470b",
+    "url": "/ash-sp-cole/lastStandTavern/static/js/runtime-main.f8be21ff.js"
+  },
+  {
+    "revision": "d6af0d1e7a5311bf484a10f517a82eb6",
+    "url": "/ash-sp-cole/lastStandTavern/static/media/bulb.d6af0d1e.jpg"
+  },
+  {
+    "revision": "beccee006fe4b1b6382cf83e1095303f",
+    "url": "/ash-sp-cole/lastStandTavern/static/media/logo.beccee00.png"
+  },
+  {
+    "revision": "3bbd746ac3f5ce733087332c384eb06a",
+    "url": "/ash-sp-cole/lastStandTavern/static/media/menu1.3bbd746a.jpg"
+  },
+  {
+    "revision": "7b77a00d5c7f0fe1d40ba50e5af64d41",
+    "url": "/ash-sp-cole/lastStandTavern/static/media/milkDrop.7b77a00d.jpg"
+  },
+  {
+    "revision": "021da895d1d6140488cf748fcc7fb501",
+    "url": "/ash-sp-cole/lastStandTavern/static/media/porch.021da895.jpg"
+  },
+  {
+    "revision": "ff69bfffd319b31a37f86a70875ce5c5",
+    "url": "/ash-sp-cole/lastStandTavern/static/media/table.ff69bfff.jpg"
+  }
+]);
